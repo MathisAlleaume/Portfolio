deed390797031ab9a467297a0ea0f6dafd9d5255
Mathis ALLEAUME <mathis.alleaume@orange.fr>
2020-07-15 09:49:07 +0200

    Page Skills en cours (bug swiper - affichage)

--- script/app.js
+++ script/app.js
@@ -10,9 +10,13 @@ window.addEventListener('scroll', () => {
             var htAbout = $('#about').height(); // height of div1 in pixels
             var osDiplomas = $('#diplomas').offset().top; // pixels to the top of div1
             var htDilpomas = $('#diplomas').height(); // height of div1 in pixels
+            var osSkills = $('#skills').offset().top; // pixels to the top of div1
+            var htSkills = $('#skills').height(); // height of div1 in pixels
+            var osExp = $('#experiences').offset().top; // pixels to the top of div1
+            var htExp = $('#experiences').height(); // height of div1 in pixels
             // if you've scrolled further than the top of div1 plus it's height
             // change the color. either by adding a class or setting a css property
-            if(scroll > osAbout + htAbout  && scroll < osDiplomas + htDilpomas){
+            if((scroll > osAbout + htAbout  && scroll < osDiplomas + htDilpomas)  || (scroll > osExp + htExp  && scroll < osSkills + htSkills)){
                 nav.classList.add('whiteNav');
             }
             else{
